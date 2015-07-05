eb4028b597095207805f88be2f817bb3a897ef07
Steven Selph <steven.selph@gmail.com>
2015-07-05 09:32:49 -0400

    Fix issue with hasher not handling errors correctly.

--- ds/hasher.go
+++ ds/hasher.go
@@ -1,6 +1,7 @@
 package ds
 
 import (
+	"fmt"
 	"hash"
 	"sync"
 
@@ -21,31 +22,49 @@ type Hasher struct {
 func (h *Hasher) Hash(p string) (string, error) {
 	chash, ok := h.c.Get(p)
 	if ok {
-		return chash.(string), nil
+		switch chash := chash.(type) {
+		default:
+			return "", fmt.Errorf("unexpected type %T", chash)
+		case string:
+			return chash, nil
+		case error:
+			return "", chash
+		}
 	}
-	h.cl.Lock()
-	hl, ok := h.l[p]
+	hl, ok := h.getPathLock(p)
 	if ok {
-		h.cl.Unlock()
 		hl.Lock()
 		hl.Unlock()
 		return h.Hash(p)
 	}
-	hl = &sync.Mutex{}
-	hl.Lock()
-	h.l[p] = hl
-	h.cl.Unlock()
+	defer h.deletePathLock(p)
 	phash, err := rh.Hash(p, h.h())
 	if err != nil {
-		hl.Unlock()
+		h.c.Add(p, err)
 		return "", err
 	}
 	h.c.Add(p, phash)
-	hl.Unlock()
-	h.cl.Lock()
-	delete(h.l, p)
-	h.cl.Unlock()
 	return phash, nil
+}
+
+func (h *Hasher) getPathLock(p string) (*sync.Mutex, bool) {
+	h.cl.Lock()
+	defer h.cl.Unlock()
+	hl, ok := h.l[p]
+	if !ok {
+		hl = &sync.Mutex{}
+		hl.Lock()
+		h.l[p] = hl
+	}
+	return hl, ok
+}
+
+func (h *Hasher) deletePathLock(p string) {
+	h.cl.Lock()
+	defer h.cl.Unlock()
+	hl := h.l[p]
+	hl.Unlock()
+	delete(h.l, p)
 }
 
 // NewHasher creates a new Hasher that hashes using the provided hash.
---
+++ ds/hasher_test.go
@@ -0,0 +1,92 @@
+package ds
+
+import(
+"crypto/sha1"
+"io/ioutil"
+"os"
+"path/filepath"
+"sync"
+"testing"
+)
+
+type File struct {
+	Path string
+	SHA1 string
+}
+
+type Data struct {
+	Dir   string
+	Files []File
+}
+
+func (d *Data) Close() error {
+	return os.RemoveAll(d.Dir)
+}
+
+func New() (*Data, error) {
+	var err error
+	data := &Data{}
+	dir, err := ioutil.TempDir("", "roms")
+	if err != nil {
+		return data, err
+	}
+	data.Dir = dir
+	defer func() {
+		if err != nil {
+			data.Close()
+		}
+	}()
+	// Bin Extensions
+	binHash := "2c2ceccb5ec5574f791d45b63c940cff20550f9a"
+	p := filepath.Join(dir, "test.bin")
+	f, err := os.Create(p)
+	if err != nil {
+		return data, err
+	}
+	for i := 0; i < 100; i++ {
+		_, err = f.Write(make([]byte, 1024*1024))
+		if err != nil {
+			return data, err
+		}
+	}
+	data.Files = append(data.Files, File{Path: p, SHA1: binHash})
+	return data, nil
+}
+
+func TestHasher(t *testing.T) {
+	d, err := New()
+	if err != nil {
+		t.Error(err)
+	}
+	defer d.Close()
+	d.Files = append(d.Files, File{Path: "doesnotexist"})
+	h, err := NewHasher(sha1.New)
+	if err != nil {
+		t.Error(err)
+	}
+	var wg sync.WaitGroup
+	f := func(f File) {
+		defer wg.Done()
+		s, err := h.Hash(f.Path)
+		if f.SHA1 != "" && s != f.SHA1 {
+			t.Errorf("h.Hash(%q) => %q; want %q", f.Path, s, f.SHA1)
+		} else if f.SHA1 == "" && err == nil {
+			t.Errorf("h.Hash(%q) => err = nil; want !nil", f.Path)
+		}
+	}
+	wg.Add(10)
+	go f(d.Files[0])
+	go f(d.Files[0])
+	go f(d.Files[0])
+	go f(d.Files[0])
+	go f(d.Files[0])
+	go f(d.Files[1])
+	go f(d.Files[1])
+	go f(d.Files[1])
+	go f(d.Files[1])
+	go f(d.Files[1])
+	wg.Wait()
+	wg.Add(2)
+	f(d.Files[0])
+	f(d.Files[1])
+}
