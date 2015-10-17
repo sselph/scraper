295cc8b6c5a1c06376b7f5ac162103cbe227334f
Steven Selph <steven.selph@gmail.com>
2015-10-16 20:32:24 -0400

    Add hash utility

---
+++ shasum/shasum.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/sselph/scraper/ds"
+)
+
+func main() {
+	files := os.Args[1:]
+	hasher, err := ds.NewHasher(sha1.New)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, file := range files {
+		fi, err := os.Stat(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if fi.IsDir() {
+			continue
+		}
+		h, err := hasher.Hash(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%s  %s\n", h, file)
+	}
+}
