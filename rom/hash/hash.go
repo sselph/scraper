e0d0e5eba156437d9073995c5bbee35da61a0351
Steven Selph <steven.selph@gmail.com>
2016-04-23 01:21:53 -0400

    Resolve #72 #78 #80, Add extensions for #43 #58 #79

--- rom/hash/hash.go
+++ rom/hash/hash.go
@@ -256,7 +256,7 @@ func decodeSNES(f io.ReadCloser, s int64) (io.ReadCloser, error) {
 
 func getDecoder(ext string) (decoder, bool) {
 	switch strings.ToLower(ext) {
-	case ".bin", ".a26", ".rom", ".cue", ".gdi", ".gb", ".gba", ".gbc", ".lyx", ".32x", ".gg", ".pce", ".sms":
+	case ".bin", ".a26", ".rom", ".cue", ".gdi", ".gb", ".gba", ".gbc", ".lyx", ".32x", ".gg", ".pce", ".sms", ".col", ".ngp", ".ngpc", ".sg", ".int":
 		return noop, true
 	case ".lnx":
 		return decodeLNX, true
