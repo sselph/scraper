bd2483e8570551fd8ba19f94cc8adc2cb1f2514d
Steven Selph <steven.selph@gmail.com>
2016-04-23 02:30:05 -0400

    Attempt to fix #74

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ev
 if [ "${GIMME_OS}" = "linux" ] && [ "${GIMME_ARCH}" = "amd64" ]; then
-	go test -v ./ds
-	go test -v ./rom/hash
+	go test -v -tags noasm ./ds
+	go test -v -tags noasm ./rom/hash
 fi
