644f29f945c7b482d1a67806bb63498a4c271ac0
Steven Selph <steven.selph@gmail.com>
2017-04-23 02:44:40 +0200

    Add .ctg extension
    Updates: #143

--- rom/hash/hash.go
+++ rom/hash/hash.go
@@ -319,7 +319,7 @@ func getDecoder(ext string) (decoder, bool) {
 	switch ext {
 	case ".bin", ".a26", ".a52", ".rom", ".cue", ".gdi", ".gb", ".gba", ".gbc", ".32x", ".gg",
 		".pce", ".sms", ".col", ".ngp", ".ngc", ".sg", ".int", ".vb", ".vec", ".gam", ".j64",
-		".jag", ".mgw", ".nds", ".fds":
+		".jag", ".mgw", ".nds", ".fds", ".ctg":
 		return noop, true
 	case ".a78":
 		return decodeA78, true
