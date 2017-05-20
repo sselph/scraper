bfb3b747c81048f70f0b0dcc837178cf6f0eac77
Steven Selph <steven.selph@gmail.com>
2017-05-20 15:51:05 -0400

    Update software name.

--- ss/ss_key.go
+++ ss/ss_key.go
@@ -3,13 +3,40 @@ package ss
 import (
 	"crypto/aes"
 	"crypto/cipher"
+	"crypto/rand"
 	"encoding/base64"
 	"encoding/json"
 	"errors"
+	"io"
 )
 
 const key = "Ym+lFlDfNS/Ud2kRTitrzA54qKBlAru5vD3k6fUqrCc="
-const data = "RumO8MHyiHBKM8DVIdehXUJfZUiLNOBJp2S/3ZBynxqW5Xo4MM2Eowjz3kf7OtMkjXwG8myC+0Fk2hxUZjvaSjqi7gT4FYZe92HqrfV8dkGlLuutWpMBlKCuJhAdLeQMou0="
+const data = "tyaeVitrQtwVtlZdKN1htjwLYrLgSezPt2edn8o1ayryAQmCHnIK3BKCbGZfMA+5FGbFMrgR1UD1p+vHkuUzgby/oR0kKzHcryJe/2OuzjMsy6K7CYaCCWzFWPny0i8XU3w="
+
+func ObfuscateDevInfo(info DevInfo) (string, error) {
+	k, err := base64.StdEncoding.DecodeString(key)
+        if err != nil {
+                return "", err
+        }
+	b, err := json.Marshal(info)
+	if err != nil {
+		return "", err
+	}
+	block, err := aes.NewCipher(k[:])
+        if err != nil {
+                return "", err
+        }
+        gcm, err := cipher.NewGCM(block)
+        if err != nil {
+                return "", err
+        }
+	nonce := make([]byte, 12)
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(err.Error())
+	}
+	ciphertext := gcm.Seal(nil, nonce, b, nil)
+	return base64.StdEncoding.EncodeToString(append(nonce, ciphertext...)), nil
+}
 
 func DeobfuscateDevInfo() (info DevInfo, err error) {
 	out := DevInfo{}
